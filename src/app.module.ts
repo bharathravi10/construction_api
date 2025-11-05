513d5f8f10f904d4ae50444ed75ac5764b4f97aa
Bharath R <118172941+bharathravi10@users.noreply.github.com>
2025-11-05 22:08:24 +0530

    Development (#11)
    * initial commit for login (#4)
    
    * updated the code make a deploy file
    
    * created the proper api for user and product (#7)
    
    Co-authored-by: Bharath <bharath.ravi@vvtsolutions.in>
    
    * resolved the build error
    
    * initial commit for assigning tasks (#10)
    
    ---------
    
    Co-authored-by: Bharath <bharath.ravi@vvtsolutions.in>

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { UserDetailsModule } from './modules/user-details/user-details.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ProjectsModule } from './modules/projects/projects.module';
 import { MaterialsModule } from './modules/materials/materials.module';
+import { TasksModule } from './modules/tasks/tasks.module';
 
 @Module({
   imports: [
@@ -26,7 +27,7 @@ import { MaterialsModule } from './modules/materials/materials.module';
     RolesModule,
     UserDetailsModule,
     MaterialsModule,
-    AuthModule,
+    TasksModule,
   ],
 })
 export class AppModule {}
